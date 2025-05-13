850044a69d7d1cc9f0dd29552a8df1bbcc6f5197
Deepak <deepak.jindal2001@gmail.com>
2025-05-13 18:06:53 +0530

    delete item on press

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,22 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const GoalItem = (props) => {
   return (
     <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   goalItem: {
-    padding: 8,
     marginBottom: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
 
